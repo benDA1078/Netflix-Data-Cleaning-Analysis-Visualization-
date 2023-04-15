8f1f5a8036a503649384bfa5e9f7f247e873bd9e
J <112655623+benDA1078@users.noreply.github.com>
2023-04-15 13:27:00 -0500

    Update Netflix.R

--- Netflix.R
+++ Netflix.R
@@ -1,2 +1,24 @@
-glimpse(Netflix)
-table(Netflix$type)
+``` {r}
+  glimpse(Netflix)
+  table(Netflix$type)
+```
+
+```{r}
+  png("Movie & TV Shows", width = 1280, height = 1280)
+    Netflix %>% 
+      count(type) %>%
+      ggplot() + geom_col(aes(x = type, y= n, fill = type)) + 
+      labs(title = "Movie & TV Shows", subtitle = "Netflix Data", caption = "Data Source: Kaggle, *Shivam Bansal*") + theme_update()
+    dev.of()
+```
+
+```{r}
+png("Total Movies & TV Shows available", width = 1280, height = 1280)
+    Netflix %>%
+      ggplot(aes(type, fill=type)) + 
+      geom_bar() + 
+      coord_flip() +
+      geom_text(stat = "count", aes(label = ..count..),hjust=1.5) + 
+      theme_update()
+    dev.off()
+```
